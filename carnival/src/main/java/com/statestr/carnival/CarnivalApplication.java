976245e0d7f540f065ce5eff5255e935b4456e8c
Carnival-win\carnival <dwu1@statestreet.com>
2023-09-14 05:37:18 +0000

    init

---
+++ carnival/src/main/java/com/statestr/carnival/CarnivalApplication.java
@@ -0,0 +1,13 @@
+package com.statestr.carnival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnivalApplication.class, args);
+	}
+
+}
---
+++ carnival/src/test/java/com/statestr/carnival/CarnivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statestr.carnival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
